a735f14a22c8c4ae09069c786baa2f991e56b952
MrChuse <aleksha007@mail.ru>
2019-11-22 16:24:47 +0300

    Update local_client.py
    Added show and help

--- local_client.py
+++ local_client.py
@@ -1,30 +1,41 @@
 from core import TheGame
 
+def print_help():
+    print('To play, write the command into the console')
+    print('List of commands:')
+    print('help - print this message')
+    print('show - show the field again')
+    print('place coord_x coord_y - place your virus to place (coord_x, coord_y)')
+    print('kill coord_x, coord_y - kill an enemy in place (coord_x, coord_y)')
+    print('pass - pass your turn')
+
 g = TheGame()
 players = (1, 2)
 turn = 0
+print('to get help enter "help"')
 g.show()
 while not g.is_over():
     inp = input(f'Player {players[0]}, enter your move:').split()
-    if inp[0] == 'pass':
-        move = 2
-        x = 0
-        y = 0
+    if inp[0] == 'help':
+        print_help()
+        continue
+    elif inp[0] == 'show':
+        g.show()
+        continue
     else:
-        move, x, y = inp
+        move, rest = inp[0], list(map(int, inp[1:]))
     if move == 'place':
         move = 0
     elif move == 'kill':
         move = 1
     elif move == 'pass':
         move = 2
-    x, y = int(x), int(y)
     try:
-        g.make_a_move(players[0], (move, (x, y)))
+        g.make_a_move(players[0], (move, rest))
     except Exception as e:
         print(e)
     else:
-        if move == 2:
+        if move_id == 2:
             turn = 3
         else:
             turn += 1
