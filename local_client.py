36bde3beea4c6126b97d4e37eb3a0fac5f52593e
MrChuse <aleksha007@mail.ru>
2019-11-22 16:44:31 +0300

    Update local_client.py
    added argparse

--- local_client.py
+++ local_client.py
@@ -1,5 +1,5 @@
 from core import TheGame
-
+import argparse
 def print_help():
     print('To play, write the command into the console')
     print('List of commands:')
@@ -8,6 +8,11 @@ def print_help():
     print('place coord_x coord_y - place your virus to place (coord_x, coord_y)')
     print('kill coord_x, coord_y - kill an enemy in place (coord_x, coord_y)')
     print('pass - pass your turn')
+
+parser = argparse.ArgumentParser(description='virus war - small strategy paper game')
+parser.add_argument('--field-width', type=int, default=10, help='width of the standart field (default: 10)')
+parser.add_argument('--players-number', type=int, default=2, choices=[2, 4], help='number of players: 2 or 4 (default: 2)')
+args = parser.parse_args()
 
 g = TheGame()
 players = (1, 2)
@@ -22,6 +27,8 @@ while not g.is_over():
     elif inp[0] == 'show':
         g.show()
         continue
+    elif inp[0] == 'end' or inp[0] == 'break' or inp[0] == 'exit':
+        break
     else:
         move, rest = inp[0], list(map(int, inp[1:]))
     if move == 'place':
@@ -35,7 +42,7 @@ while not g.is_over():
     except Exception as e:
         print(e)
     else:
-        if move_id == 2:
+        if move == 2:
             turn = 3
         else:
             turn += 1
@@ -43,8 +50,10 @@ while not g.is_over():
     if turn == 3:
         players = (players[1], players[0])
         turn = 0
-winner = g.winner()
-if winner == -1:
-    print('Draw!')
+
 else:
-    print('Player', winner, 'won')
+    winner = g.winner()
+    if winner == -1:
+        print('Draw!')
+    else:
+        print('Player', winner, 'won')
